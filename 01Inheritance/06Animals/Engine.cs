2d6d5cf8b169843af1b5cc217c7dd8f14014ce53
Rositsa Nenova <93535482+NenovaRositsa@users.noreply.github.com>
2021-11-09 12:28:56 +0200

    Added solutions

---
+++ 01Inheritance/06Animals/Engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Animals
+{
+    public class Engine
+    {
+        private List<Animal> animals;
+
+        public Engine()
+        {
+            this.animals = new List<Animal>();
+        }
+        public void Run()
+        {
+            string animaltype;
+
+            while ((animaltype = Console.ReadLine()) != "Beast!")
+            {
+                string[] input = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string gender = input[2];
+
+
+
+
+            }
+        }
+    }
+}
